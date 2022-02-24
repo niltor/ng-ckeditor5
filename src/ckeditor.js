1a9b052e39d4b7b43aa82a8f39d448677e07b047
niltor <zpty@outlook.com>
2022-02-24 16:12:35 +0800

    add markdown plugin

--- src/ckeditor.js
+++ src/ckeditor.js
@@ -33,6 +33,7 @@ import TextTransformation from '@ckeditor/ckeditor5-typing/src/texttransformatio
 import CloudServices from '@ckeditor/ckeditor5-cloud-services/src/cloudservices';
 import SimpleUploadAdapter from '@ckeditor/ckeditor5-upload/src/adapters/simpleuploadadapter';
 import CodeBlock from '@ckeditor/ckeditor5-code-block/src/codeblock';
+import Markdown from '@ckeditor/ckeditor5-markdown-gfm/src/markdown';
 
 export default class ClassicEditor extends ClassicEditorBase { }
 
@@ -64,7 +65,8 @@ ClassicEditor.builtinPlugins = [
 	TextTransformation,
 	CodeBlock,
 	Code,
-	SimpleUploadAdapter
+	SimpleUploadAdapter,
+	// Markdown
 ];
 
 // Editor configuration.
