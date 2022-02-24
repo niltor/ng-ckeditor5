521fa1454278731d1d3222fd7da0d1f130fc67e5
niltor <zpty@outlook.com>
2022-02-24 15:09:08 +0800

    add codeblock

--- src/ckeditor.js
+++ src/ckeditor.js
@@ -11,6 +11,7 @@ import UploadAdapter from '@ckeditor/ckeditor5-adapter-ckfinder/src/uploadadapte
 import Autoformat from '@ckeditor/ckeditor5-autoformat/src/autoformat';
 import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold';
 import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic';
+import Code from '@ckeditor/ckeditor5-basic-styles/src/code';
 import BlockQuote from '@ckeditor/ckeditor5-block-quote/src/blockquote';
 import CKFinder from '@ckeditor/ckeditor5-ckfinder/src/ckfinder';
 import EasyImage from '@ckeditor/ckeditor5-easy-image/src/easyimage';
@@ -31,6 +32,7 @@ import TableToolbar from '@ckeditor/ckeditor5-table/src/tabletoolbar';
 import TextTransformation from '@ckeditor/ckeditor5-typing/src/texttransformation';
 import CloudServices from '@ckeditor/ckeditor5-cloud-services/src/cloudservices';
 import SimpleUploadAdapter from '@ckeditor/ckeditor5-upload/src/adapters/simpleuploadadapter';
+import CodeBlock from '@ckeditor/ckeditor5-code-block/src/codeblock';
 
 export default class ClassicEditor extends ClassicEditorBase { }
 
@@ -60,6 +62,8 @@ ClassicEditor.builtinPlugins = [
 	Table,
 	TableToolbar,
 	TextTransformation,
+	CodeBlock,
+	Code,
 	SimpleUploadAdapter
 ];
 
@@ -75,8 +79,8 @@ ClassicEditor.defaultConfig = {
 			'bulletedList',
 			'numberedList',
 			'|',
-			'outdent',
-			'indent',
+			'code',
+			'codeBlock',
 			'|',
 			'uploadImage',
 			'blockQuote',
@@ -84,6 +88,16 @@ ClassicEditor.defaultConfig = {
 			'mediaEmbed',
 			'undo',
 			'redo'
+		],
+		shouldNotGroupWhenFull: true
+	},
+	codeBlock:{
+		languages: [
+			{ language: 'csharp', label: 'C#' },
+			{ language: 'javascript', label: 'JS' },
+			{ language: 'typescript', label: 'TS' },
+			{ language: 'css', label: 'CSS' },
+			{ language: 'html', label: 'HTML' }
 		]
 	},
 	image: {
